d2b714acfb6a4674ffd5713a6c80fe528619e848
zachary <zking4540@gmail.com>
2025-11-27 14:57:17 +0100

    removing vet app

--- vetapp/animal/context_processors.py
+++
@@ -1,9 +0,0 @@
-def user_role(request):
-    if request.user.is_authenticated:
-        is_vet = request.user.is_superuser
-        is_secretary = request.user.groups.filter(name='Secretaries').exists()
-        return {
-            'user_is_vet': is_vet,
-            'user_is_secretary': is_secretary,
-        }
-    return {}
